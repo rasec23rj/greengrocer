f2284d43e36ae38873ac86f1bc157da2584a595c
julio <jcbarcelosrj@gmail.com>
2025-12-03 16:03:34 -0300

    add toast

--- lib/src/services/util_services.dart
+++ lib/src/services/util_services.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/painting.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class UtilServices {
   static String priceToCurrency(double price) {
@@ -23,5 +25,21 @@ class UtilServices {
     initializeDateFormatting();
     DateFormat dateFormat = DateFormat.yMd('pt_BR').add_Hm();
     return dateFormat.format(dateTime);
+  }
+
+  void showToast({required String message, bool isError = false}) {
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 3,
+      backgroundColor: isError
+          ? Color.fromARGB(255, 248, 2, 2)
+          : Color.fromARGB(255, 255, 254, 254),
+      textColor: isError
+          ? Color.fromARGB(255, 255, 255, 255)
+          : Color.fromARGB(255, 7, 7, 7),
+      fontSize: 14,
+    );
   }
 }
