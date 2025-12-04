5ca84e5a0cb613a84d3c1453b9d405301cfc7264
julio <jcbarcelosrj@gmail.com>
2025-12-04 11:23:01 -0300

    feat: create validetors cpf, phone

--- lib/src/services/validators.dart
+++ lib/src/services/validators.dart
@@ -7,3 +7,31 @@ String? emailValidator(String? email) {
   if (!email.isEmail) return 'Digite um email válido';
   return null;
 }
+
+String? passwordValidator(password) {
+  if (password == null || password.isEmpty) {
+    return 'Digite uma senha';
+  }
+  if (password.length <= 7)
+    return 'Digite uma senha com no minimo de 7. carcteres';
+
+  return null;
+}
+
+String? phoneValidator(phone) {
+  if (phone == null || phone.isEmpty) {
+    return 'Digite uma telefone';
+  }
+  if (!phone.isPhoneNumber) return 'Digite um telefone inválido';
+
+  return null;
+}
+
+String? cpfValidator(cpf) {
+  if (cpf == null || cpf.isEmpty) {
+    return 'Digite uma cpf';
+  }
+  if (!cpf.isCpf) return 'Digite um cpf inválido';
+
+  return null;
+}
